6381f270fe9017117abaad99c5c010d030ba8ca7
minsku <minja98@hotmail.com>
2022-03-21 09:15:07 +0200

    http-c ready

--- src/hooks/ApiHooks.js
+++ src/hooks/ApiHooks.js
@@ -1,25 +1,46 @@
-import {useEffect, useState} from 'react';
-import {baseUrl} from '../utils/variables';
+// TODO: add necessary imports
+import { useEffect, useState } from "react";
+import { baseUrl } from "../utils/variables";
+
+
+const fetchJson = async (url, options = {}) => {
+  try {
+    const response = await fetch(url, options);
+    const json = await response.json();
+    if (response.ok) {
+    return json;
+    } else {
+      const message = json.error;
+      throw new Error(message);
+    }
+  } catch (err) {
+    throw new Error(err.message);
+  }
+
+};
 
 const useMedia = () => {
+  // TODO: move mediaArray state here
+  // TODO: move loadMedia function here
+  // TODO: move useEffect here
+
   const [mediaArray, setMediaArray] = useState([]);
+
   const getMedia = async () => {
     try {
-      const mediaResponse = await fetch(baseUrl + 'media');
-      const media = await mediaResponse.json();
-      const allFiles = await Promise.all(
-        media.map(async (file) => {
-          const fileResponse = await fetch(`${baseUrl}media/${file.file_id}`);
-          return await fileResponse.json();
-        })
-      );
-      setMediaArray(allFiles);
+   const media = await fetchJson(baseUrl + 'media');
+   const allFiles = await Promise.all(media.map(async (file)=>{
+     return await fetchJson(`${baseUrl}media/${file.file_id}`);
+   })
+   );
+   setMediaArray(allFiles);
     } catch (err) {
       console.error(err.message);
     }
+
   };
 
-  useEffect(() => {
+  useEffect( () => {
     getMedia();
   }, []);
   return {mediaArray};
